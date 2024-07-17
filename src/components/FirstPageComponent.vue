101534c2231d7782c5c5c544083f5cea71e8e052
ahamad236 <ahmadshaik4u@gmail.com>
2024-07-17 14:37:01 +0530

    first commit

---
+++ src/components/FirstPageComponent.vue
@@ -0,0 +1,37 @@
+<!-- <template>
+    <h2>
+        First Page Component
+    </h2>
+    <div>
+        userName in Child : {{ model }}
+    </div>
+    <input v-model="model"/>
+</template>
+
+<script setup lang="ts">
+    const [model, modifiers] = defineModel({
+        set(value:string){
+            if(modifiers.capitalize){
+                return value.charAt(0).toUpperCase()+value.slice(1)
+            }
+            return value
+        }
+    })
+</script> -->
+
+
+
+<!-- v-model modifiers with arguments starts from here -->
+<template>
+    <h2> First Page Component </h2>
+    <input v-model="firstName"/>
+    <input v-model="lastName"/>
+</template>
+<script setup lang="ts">
+    const [firstName, firstNameModifiers] = defineModel('firstName')
+    const [lastName, lastNameModifiers] = defineModel('lastName')
+
+    console.log(firstNameModifiers.capitalize)
+    console.log(lastNameModifiers.upperCase)
+</script>
+<!-- v-model modifiers upto above -->
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,33 @@
+<!-- <template>
+    <h1>
+        Home Component
+    </h1>
+    <div>userName in Parent : {{userName}}</div>
+    <input v-model="userName"/>
+    <FirstPage v-model.capitalize="userName"></FirstPage>
+</template>
+
+<script setup lang="ts">
+    import FirstPage from "./FirstPageComponent.vue"
+    import { ref } from 'vue'
+    // import type { Ref } from 'vue'
+
+    const userName = ref()
+</script> -->
+
+
+
+<!-- v-model modifiers with arguments starts from here -->
+<template>
+    
+    <FirstPage v-model.first-name.capitalize="first" v-model.last-name.upperCase="last"></FirstPage>
+</template>
+
+<script setup lang="ts">
+    import FirstPage from "./FirstPageComponent.vue"
+    import { ref } from 'vue'
+    
+    const first = ref('hai')
+    const last = ref('hello')
+</script>
+<!-- v-model modifiers upto above -->
